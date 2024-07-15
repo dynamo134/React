7e1956ef3aba256355430e85b8d83b2d03fdd1ce
dynamo134 <shaharyaramu16@gmail.com>
2024-07-15 18:24:20 +0530

    React-Router

---
+++ 05ReactRouter/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      {/* <Header/>
+      <Home/>
+      <Footer/> */}
+    </>
+  )
+}
+
+export default App
